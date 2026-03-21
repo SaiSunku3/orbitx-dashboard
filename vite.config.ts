caa8fe5eab333bc5ba2f295862b2c8902d7369ad
SaiSunku3 <sai.sunku@gmail.com>
2026-03-21 10:02:36 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: '/',  // already there? good
   plugins: [react(), tailwindcss()],
