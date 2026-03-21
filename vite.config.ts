5844673d023985dde492f663c2a2c962d622545f
SaiSunku3 <sai.sunku@gmail.com>
2026-03-21 13:34:01 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,17 +4,4 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-
-  // This helps Vercel / Rollup with client-only libraries
-  build: {
-    rollupOptions: {
-      external: [
-        '@emailjs/browser',
-        'zustand',
-        'lucide-react',
-        'recharts',
-        'framer-motion'
-      ]
-    }
-  }
 })
