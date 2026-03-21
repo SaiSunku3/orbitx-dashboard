18f150fd9da90bb7d590aa99026e45e12bd801fd
SaiSunku3 <sai.sunku@gmail.com>
2026-03-21 17:54:26 -0500

    Update Settings.tsx

--- src/Pages/Settings.tsx
+++ src/Pages/Settings.tsx
@@ -2,7 +2,7 @@ export default function Settings() {
   return (
     <div className="text-center py-20">
       <h1 className="text-5xl font-bold mb-6">Settings</h1>
-      <p className="text-xl text-gray-400">Theme switch, animation controls, and EmailJS test coming in next update.<br />Everything else is already live!</p>
+      <p className="text-xl text-gray-400">Theme switch, animation controls, and EmailJS test ,coming in next update.<br />Everything else is already live!</p>
     </div>
   )
 }
