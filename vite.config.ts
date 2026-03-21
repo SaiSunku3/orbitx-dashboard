b6a2fa72ba59c1707548c12ba6d4ed8c9175dbf9
SaiSunku3 <sai.sunku@gmail.com>
2026-03-21 13:41:57 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { visualizer } from 'rollup-plugin-visualizer'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(),visualizer({ open: true })],
 
   build: {
     chunkSizeWarningLimit: 1000,   // or 800 — suppresses warning until you get bigger
   },
 })
+
+
+
